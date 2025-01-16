2c9975562ef1fce76dba3a378151d363b09a908b
testing.engineer <testing.engineer@GAVLMUMDT-046.godrejinds.com>
2025-01-16 17:42:02 +0530

    this third push

---
+++ samadhan_Procurement_Farmer/src/main/java/ObjectoRepository_Farmer/profilePicturePage.java
@@ -0,0 +1,39 @@
+package ObjectoRepository_Farmer;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class profilePicturePage {
+	
+	AndroidDriver driver;
+	public profilePicturePage(AndroidDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//android.widget.TextView[@resource-id=\"button-text\" and @text=\"Gallery\"]")
+	private WebElement galleryTab;
+	
+	@FindBy(xpath = "//android.widget.TextView[@resource-id=\"button-text\" and @text=\"Camera\"]")
+	private WebElement cameraTab;
+	
+	@FindBy(xpath = "//com.horcrux.svg.PathView")
+	private WebElement cancelBtn;
+
+	public WebElement getGalleryTab() {
+		return galleryTab;
+	}
+
+	public WebElement getCameraTab() {
+		return cameraTab;
+	}
+
+	public WebElement getCancelBtn() {
+		return cancelBtn;
+	}
+	
+	
+}
